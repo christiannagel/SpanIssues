e2778a8a1aab17105d6cace80b0a1d2735925857
Christian Nagel <christian@christiannagel.com>
2017-06-27 17:37:59 +0200

    Span<T>.ToString

---
+++ SpanTests/SpanTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpanTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello, World!";
+            ReadOnlySpan<char> span = s.AsSpan();
+            string s2 = span.ToString();  // exception: Unhandled Exception: System.InvalidProgramException: Common Language Runtime detected an invalid program.
+        }
+    }
+}
